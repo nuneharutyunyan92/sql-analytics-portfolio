6b5f6de95e67d8dc80919df8b27bdda399942d72
nuneharutyunyan92 <nune.harutyunyan92@gmail.com>
2026-03-12 21:27:56 +0400

    add updates

--- reset-db.ps1
+++ reset-db.ps1
@@ -1,13 +1,13 @@
+
 $ErrorActionPreference = "Stop"  # Exit immediately if any command fails
-Write-Host "Stopping containers... and removing volumes..."
-docker compose down -v
+Write-Host "Stopping containers..."
+docker compose down
 Write-Host "Removing Postgres data directory..."
 Remove-Item -Recurse -Force .\postgres_data -ErrorAction SilentlyContinue
-Write-Host "Removing Pgadmin data directory..."
-Remove-Item -Recurse -Force .\pgadmin_data -ErrorAction SilentlyContinue
 Write-Host "Starting containers..."
-docker compose up --build
+docker compose up
 Write-Host "Database reset complete. Init scripts reran successfully."
 Collapse
 
 # powershell -ExecutionPolicy Bypass -File reset-db.ps1
+# տեստ
